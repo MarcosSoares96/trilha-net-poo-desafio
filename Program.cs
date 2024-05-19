4e33cf4fe3aa88f350faa7c4e892e8ab36bca510
Marcos Santos <marcos_santos_soares@hotmail.com>
2024-05-19 13:02:43 -0300

    Finalizando Projeto.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizar os testes com as classes Nokia e Iphone]
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("37652411");
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone iphone = new Iphone("75716938");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
+
+Console.ReadLine();
